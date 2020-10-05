949c0b6d51d18f8b6125ef91e607fe07731ba4e5
tradityaxx <tradityaxx@gmail.com>
2020-10-05 12:56:44 +0300

    Update models and report

--- analysis.R
+++ analysis.R
@@ -46,12 +46,15 @@ rmse <- function(pred_rating, actual_rating){
   sqrt(mean((pred_rating - actual_rating)^2))
 }
 
-# SIMPLE MODEL
+#------------------------------
+
+# 1. SIMPLE MODEL
 mu <- mean(train_set$rating)
 
 model_1_rmse <- rmse(mu, test_set$rating)
+#------------------------------
 
-# ADD MOVIE EFFECTS 
+# 2. ADD MOVIE EFFECTS 
 movie_avg <- train_set %>% 
   group_by(movieId) %>%
   summarise(b_i = mean(rating - mu)) 
@@ -63,8 +66,9 @@ b_i <- test_set %>%
 y_hat <- mu + b_i
 
 model_2_rmse <- rmse(y_hat, test_set$rating)  
+#------------------------------
 
-# ADD USER EFFECTS
+# 3. ADD USER EFFECTS
 user_avg <- train_set %>%
   group_by(userId) %>% 
   left_join(movie_avg, by = "movieId") %>%
@@ -77,8 +81,9 @@ b_u <- test_set %>%
 y_hat <- mu + b_i + b_u
 
 model_3_rmse <- rmse(y_hat, test_set$rating)
+#------------------------------
 
-# ADD DAY EFFECT
+## ADD DAY EFFECT
 train_set <- train_set %>% 
   mutate(date = as_datetime(timestamp))
 
@@ -121,8 +126,9 @@ test_set_daycount %>%
   left_join(user_avg, by="userId") %>%
   left_join(day_avg, by="daycount")
 # Notice there are a lot of NAs for the day effect
+#------------------------------
 
-# ADD WEEK EFFECT 
+# 4. ADD WEEK EFFECT 
 train_set_week %>% 
   mutate(week = round_date(date, unit="week"))
 
@@ -143,11 +149,12 @@ w_ui <- test_set %>%
 w_ui <- replace_na(w_ui, mu)
 
 y_hat <- mu + b_i + b_u + w_ui
+
 model_4_rmse <- rmse(test_set$rating, y_hat)
 # Week effect did improve from previous model, but not significant
+#------------------------------
 
-
-# ADD GENRE EFFECT
+# 5. ADD GENRE EFFECT
 # Plot to see the genre effect
 train_set %>% group_by(genres) %>%
   summarise(n = n(), avg = mean(rating), se = sd(rating)/sqrt(n)) %>%
@@ -157,18 +164,105 @@ train_set %>% group_by(genres) %>%
   geom_errorbar() +
   theme(axis.text.x = element_text(angle=60, hjust=1))
 
+## Still working on it
+
+#------------------------------
+
+# 6. ADD REGULARIZATION FOR MOVIE + USER 
+lambda = 0.75
+
+# Penalize movie effect
+movie_avg_reg <- train_set %>%
+  group_by(movieId) %>% 
+  summarise(n=n(), b_i_reg = sum(rating - mu) / (lambda + n) )
+
+# Penalize user effect
+user_avg_reg <- train_set %>%
+  group_by(userId) %>% 
+  left_join(movie_avg, by="movieId") %>%
+  summarise(n=n(), b_u_reg = sum(rating - mu - b_i) / (lambda + n) )
+
+b_i_reg <- test_set %>% 
+  left_join(movie_avg_reg, by="movieId") %>%
+  .$b_i_reg
+
+b_u_reg <- test_set %>% 
+  left_join(user_avg_reg, by="userId") %>%
+  .$b_u_reg
+
+y_hat <- mu + b_i_reg + b_u_reg
+
+model_6_rmse <- rmse(y_hat, test_set$rating)
+#------------------------------
+
+# 7. ADD REGULARIZATION FOR MOVIE + USER + WEEK
+week_avg_reg <- train_set_week %>% 
+  group_by(week) %>% 
+  left_join(movie_avg, by = "movieId") %>%
+  left_join(user_avg, by = "userId") %>%
+  summarise(n=n(), w_ui_reg = sum(rating - mu - b_i - b_u) / (lambda + n) )
+  
+w_ui_reg <- test_set %>% 
+  mutate(date = as_datetime(timestamp), 
+         week = round_date(date, unit="week")) %>%
+  left_join(movie_avg, by="movieId") %>%
+  left_join(user_avg, by="userId") %>%
+  left_join(week_avg_reg, by="week") %>%
+  .$w_ui_reg
+# Now there's only 1 NA when using week effect. Replace NA with mu
+w_ui_reg <- replace_na(w_ui_reg, mu)
+
+y_hat <- mu + b_i + b_u + w_ui_reg
+
+model_7_rmse <- rmse(test_set$rating, y_hat)  
+
+#------------------------------
+# CHOOSE BEST LAMBDA
+#------------------------------
+lambdas <- seq(0, 5, 0.25)
+
+rmses <- sapply(lambdas, function(lambda){
+  movie_avg_reg <- train_set %>%
+    group_by(movieId) %>% 
+    summarise(n=n(), b_i_reg = sum(rating - mu) / (lambda + n) )
+  
+  user_avg_reg <- train_set %>%
+    group_by(userId) %>% 
+    left_join(movie_avg, by="movieId") %>%
+    summarise(n=n(), b_u_reg = sum(rating - mu - b_i) / (lambda + n) )
+  
+  b_i_reg <- test_set %>% 
+    left_join(movie_avg_reg, by="movieId") %>%
+    .$b_i_reg
+  
+  b_u_reg <- test_set %>% 
+    left_join(user_avg_reg, by="userId") %>%
+    .$b_u_reg
+  
+  y_hat <- mu + b_i_reg + b_u_reg
+  
+  return(rmse(y_hat, test_set$rating))
+})
+lambda <- lambdas[which.min(rmses)]
+lambda
+
+data.frame(lambdas, rmses) %>%
+  ggplot(aes(lambdas, rmses)) + geom_point()
 
 #-------------------------------------------------------------------------------
 # COMPARE THE RESULTS
 #-------------------------------------------------------------------------------
-rmse_results <- data.frame(Method = c("Simple Model", "Movie Effect", 
-                                      "Movie + User Effect", "Movie + User + Week Effect",
-                                      "Movie + User + Genre Effect"), 
+rmse_results <- data.frame(Method = c("Simple Model", 
+                                      "Movie Effect", 
+                                      "Movie + User Effect", 
+                                      "Movie + User + Week Effect",
+                                      "Regularized Movie + User Effect",
+                                      "Regularized Movie + User + Week Effect"), 
                            RMSE = c(model_1_rmse, model_2_rmse, model_3_rmse,
-                                    model_4_rmse, model_5_rmse))
+                                    model_4_rmse, model_7_rmse, model_8_rmse))
 
 rmse_results %>% knitr::kable()
+#------------------------------
 
 
-## Genre Effect
 
